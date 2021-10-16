f31dda763db96d7eaacad81ffa4edd003a1c4622
Outsider987 <t7902195204@gmail.com>
2021-10-16 19:56:54 +0800

    add try catch

--- spider_playwright.py
+++ spider_playwright.py
@@ -81,81 +81,90 @@ class MySpider:
             await self.start_requests()
 
     async def enterRoot(self, a_href):
-        async with async_playwright() as playwright2:
-            fu = UserAgent(verify_ssl=False)
-            headers = {'user-agent': fu.firefox,
-                       #    'referer': 'https://ceomap.site/#/advertising#about'
-                       }
-            browser = await playwright2.firefox.launch(
-                headless=self.headless,
-                timeout=1000 * 80,
-                # proxy={
-                #     "server": proxy
-                # }
-            )
-            context = await browser.new_context()
-            page = await context.new_page()
-            await page.goto(a_href, timeout=1000 * 100, referer='https://www.youtube.com/')
-            await page.wait_for_selector('div.bookid_chapterBox__CRrx9', timeout=1000 * 10)
-            pTags = await page.query_selector_all('p')
-            title_h5 = await page.query_selector('h5')
-            thumbnail_urlTag = await page.query_selector('div.comicBox_thumbImg__1aomD')
-            thumbnail_url = await thumbnail_urlTag.get_attribute('data-src')
-            title = await title_h5.inner_text()
-            print(title)
-            rootStr = []
-            for index, p in enumerate(pTags):
-                if index < 4:
-                    text = await p.inner_text()
-                    ptext = text.split(':')
-                    rootStr.append(ptext[1])
-            print(rootStr)
-            descr = rootStr[3]
-            author = rootStr[0].strip()
-            status = rootStr[1]
-            genre = rootStr[2]
+        try:
+            async with async_playwright() as playwright2:
+                fu = UserAgent(verify_ssl=False)
+                headers = {'user-agent': fu.firefox,
+                        #    'referer': 'https://ceomap.site/#/advertising#about'
+                        }
+                browser = await playwright2.firefox.launch(
+                    headless=self.headless,
+                    timeout=1000 * 80,
+                    # proxy={
+                    #     "server": proxy
+                    # }
+                )
+                context = await browser.new_context()
+                page = await context.new_page()
+                await page.goto(a_href, timeout=1000 * 100, referer='https://www.youtube.com/')
+                await page.wait_for_selector('div.bookid_chapterBox__CRrx9', timeout=1000 * 10)
+                pTags = await page.query_selector_all('p')
+                title_h5 = await page.query_selector('h5')
+                thumbnail_urlTag = await page.query_selector('div.comicBox_thumbImg__1aomD')
+                thumbnail_url = await thumbnail_urlTag.get_attribute('data-src')
+                title = await title_h5.inner_text()
+                print(title)
+                rootStr = []
+                for index, p in enumerate(pTags):
+                    if index < 4:
+                        text = await p.inner_text()
+                        ptext = text.split(':')
+                        rootStr.append(ptext[1])
+                print(rootStr)
+                descr = rootStr[3]
+                author = rootStr[0].strip()
+                status = rootStr[1]
+                genre = rootStr[2]
 
-            mysql.storeRootComicsData(
-                descr, a_href, title, author, status, thumbnail_url)
+                mysql.storeRootComicsData(
+                    descr, a_href, title, author, status, thumbnail_url)
 
-            rouman_id_arrary = mysql.getData(
-                'id', 'rouman_rootcomics', 'title', title)
-            rouman_id = rouman_id_arrary[0]
-            mysql.storeGenreData(rouman_id, genre)
-            comics = await page.query_selector_all('div.bookid_chapter__20FJi')
-            chapter = 0 
-            comics,chapter = myfilter.filter_chapter(rouman_id, comics)
-            for index, comic in enumerate(comics):
-                comic_a_Tag = await comic.query_selector('a')
-                comic_href = await comic_a_Tag.get_attribute('href')
-                await self.enterComics(base + comic_href, rouman_id, index +chapter)
+                rouman_id_arrary = mysql.getData(
+                    'id', 'rouman_rootcomics', 'title', title)
+                rouman_id = rouman_id_arrary[0]
+                mysql.storeGenreData(rouman_id, genre)
+                comics = await page.query_selector_all('div.bookid_chapter__20FJi')
+                chapter = 0 
+                comics,chapter = myfilter.filter_chapter(rouman_id, comics)
+                for index, comic in enumerate(comics):
+                    comic_a_Tag = await comic.query_selector('a')
+                    comic_href = await comic_a_Tag.get_attribute('href')
+                    await self.enterComics(base + comic_href, rouman_id, index +chapter)
+        except Exception as e:
+            print(e)
+            pass
+            
 
     async def enterComics(self, comic_href, rouman_rootcomics_id, chapter):
-        async with async_playwright() as playwright3:
-            fu = UserAgent(verify_ssl=False)
-            headers = {'user-agent': fu.firefox,
-                       #    'referer': 'https://ceomap.site/#/advertising#about'
-                       }
-            browser = await playwright3.firefox.launch(
-                headless=self.headless,
-                timeout=1000 * 80,
-                # proxy={
-                #     "server": proxy
-                # }
-            )
-            context = await browser.new_context()
-            page = await context.new_page()
-            await page.goto(comic_href, timeout=1000 * 100, referer='https://www.youtube.com/')
-            await page.wait_for_selector('img.id_comicImage__2vwcn', timeout=10000)
-            comics = await page.query_selector_all('img.id_comicImage__2vwcn')
-            menus =await  page.query_selector_all('div.id_pagination__3_EI_') 
-            img_list = []
-            for comic in comics:
-                # await menus[1].hover()
-                await comic.hover()
-                time.sleep(0.02)
-                comicImg_href = await comic.get_attribute('src')
+        try:
+            async with async_playwright() as playwright3:
+                fu = UserAgent(verify_ssl=False)
+                headers = {'user-agent': fu.firefox,
+                        #    'referer': 'https://ceomap.site/#/advertising#about'
+                        }
+                browser = await playwright3.firefox.launch(
+                    headless=self.headless,
+                    timeout=1000 * 80,
+                    # proxy={
+                    #     "server": proxy
+                    # }
+                )
+                context = await browser.new_context()
+                page = await context.new_page()
+                await page.goto(comic_href, timeout=1000 * 100, referer='https://www.youtube.com/')
+                await page.wait_for_selector('img.id_comicImage__2vwcn', timeout=10000)
+                comics = await page.query_selector_all('img.id_comicImage__2vwcn')
+                menus =await  page.query_selector_all('div.id_pagination__3_EI_') 
+                img_list = []
+                for comic in comics:
+                    # await menus[1].hover()
+                    await comic.hover()
+                    time.sleep(0.02)
+                    comicImg_href = await comic.get_attribute('src')
 
-                img_list.append(comicImg_href)
+                    img_list.append(comicImg_href)
 
-            mysql.storeComicsData(rouman_rootcomics_id, img_list, chapter)
+                mysql.storeComicsData(rouman_rootcomics_id, img_list, chapter)
+        except Exception as e:
+            print(e)
+            pass
